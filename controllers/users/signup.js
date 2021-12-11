ccb062b34d411ce9323ccdb62dae3d188d13e184
Akremonium <akremonium@gmail.com>
2021-12-11 17:43:04 +0200

    5/5 steps done

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,4 +1,5 @@
 const { Conflict } = require('http-errors')
+const gravatar = require('gravatar')
 
 const { User } = require('../../models')
 
@@ -8,7 +9,8 @@ const signup = async (req, res) => {
   if (user) {
     throw new Conflict(`Email ${email} in use`)
   }
-  const newUser = new User({ email })
+  const avatarURL = gravatar.url(email)
+  const newUser = new User({ email, avatarURL })
   newUser.setPassword(password)
   newUser.save()
   res.status(201).json({
--- models/user.js
+++ models/user.js
@@ -20,6 +20,10 @@ const userSchema = Schema({
   token: {
     type: String,
     default: null,
+  },
+  avatarURL: {
+    type: String,
+    required: true
   }
 }, { versionKey: false, timestamps: true })
 
